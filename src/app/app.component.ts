29b9e252f3dcbb7ebe071b453234ae88f258e251
veliValentine <nikkevanukas1995@gmail.com>
2021-03-09 16:49:32 +0200

    feat: init loginPage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <div>
-    <h1>{{title}}</h1>
-  </div>
+    <div>
+      <h1>{{title}}</h1>
+      <loginPage></loginPage>
+    </div>
   `
 })
 export class AppComponent {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPage } from '../components/loginPage/loginPage.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPage
   ],
   imports: [
     BrowserModule,
