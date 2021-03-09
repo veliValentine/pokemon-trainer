261930260f50e74e731ef2ee038d401926fd2b33
veliValentine <nikkevanukas1995@gmail.com>
2021-03-09 16:43:35 +0200

    feat: init loginPage

---
+++ src/components/loginPage/loginPage.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'loginPage',
+  templateUrl: './loginPage.component.html'
+})
+
+export class LoginPage {
+  title = 'Login page'
+}
