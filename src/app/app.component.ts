5219d6c9a52aaaaffa90145f98db4aba51ee80f2
veliValentine <nikkevanukas1995@gmail.com>
2021-03-09 15:50:05 +0200

    chore: init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <div>
+    <h1>{{title}}</h1>
+  </div>
+  `
 })
 export class AppComponent {
   title = 'pokemon-trainer';
