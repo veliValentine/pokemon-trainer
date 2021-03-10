4c194dcc551595f5dbd179c4648070a748fe93a9
veliValentine <nikkevanukas1995@gmail.com>
2021-03-10 19:18:35 +0200

    feat: trainer page

---
+++ src/components/trainerPage/trainerPage.component.ts
@@ -0,0 +1,41 @@
+import { Component } from "@angular/core";
+import { PokemonCollected } from '../../utils/interface'
+import { getPokemons, getTrainer } from '../../utils/storageHelper.js'
+
+@Component({
+  selector: 'trainerPage',
+  templateUrl: './trainerPage.component.html',
+  styleUrls: ['./trainerPage.component.css']
+})
+// TODO redirect to pokemon page on click
+export class TrainerPage {
+  trainer: string = getTrainer();
+  collectedPokemons: Array<PokemonCollected> = getPokemons();
+  haveCollectedPokemons: boolean = this.collectedPokemons && this.collectedPokemons.length > 0;
+}
+
+
+/*
+[
+  {
+    name: 'bulbasaur',
+    src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
+    id: 1
+  },
+  {
+    name: 'ivysaur',
+    src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png',
+    id: 2
+  },
+  {
+    name: 'venusaur',
+    src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png',
+    id: 3
+  },
+  {
+    name: 'charmander',
+    src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png',
+    id: 4
+  }
+];
+*/
