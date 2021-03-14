94137c46b229743d96ca667db901f1c496693e33
veliValentine <nikkevanukas1995@gmail.com>
2021-03-14 21:18:32 +0200

    refactor: moved parseResponses to utils

--- src/services/pokemon.service.ts
+++ src/services/pokemon.service.ts
@@ -2,10 +2,11 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { map } from 'rxjs/operators'
+import { parsePokemon, parsePokemonBase } from "src/utils/parseResponses";
 
 import { PokemonBase, Pokemon, } from "../utils/interface";
 import { POKEMONS_API_URL, POKEMON_API_URL } from "./serviceHelper";
-import { parsePokemon, parsePokemonBase } from "./parseResponses";
+
 
 @Injectable({
   providedIn: 'root'
