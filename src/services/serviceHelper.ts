4521844c7fcd3bd579e57860a20d2922450d1388
veliValentine <nikkevanukas1995@gmail.com>
2021-03-15 14:14:53 +0200

    feat: get 100 pokemons

--- src/services/serviceHelper.ts
+++ src/services/serviceHelper.ts
@@ -1,4 +1,4 @@
-export const POKEMONS_API_URL: string = 'https://pokeapi.co/api/v2/pokemon'
+export const POKEMONS_API_URL: string = 'https://pokeapi.co/api/v2/pokemon?limit=100'
 
 export const POKEMON_API_URL = (id: number): string => `https://pokeapi.co/api/v2/pokemon/${id}`
 
